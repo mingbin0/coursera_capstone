0d24e1e3c43853b5b8fe7a976733080e15785df7
unknown <danieltanmb@gmail.com>
2020-05-29 09:54:03 +0800

    file for coursera capstone

---
+++ Coursera Capstone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# This notebook is used for Coursera Capstone
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course")
+
